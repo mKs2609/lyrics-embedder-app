6ce22402b84e9a0cae431cdbb193ade38ebc760a
Mohit Kumar <mohitks2609@gmail.com>
2026-05-23 04:19:24 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application") version "8.1.1"
+    id("org.jetbrains.kotlin.android") version "1.8.22"
 }
 
 android {
@@ -18,36 +18,26 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 repositories {
     google()
     mavenCentral()
-    maven { url = uri("https://jitpack.io") } // Required to resolve JitPack dependencies
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    
-    // Core tagging engine optimized for Android runtime structures
     implementation("com.github.Shabinder:JAudioTagger-Android:1.0")
-    
-    // Networking and JSON parsers
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.gson:gson:2.10.1")
-    
-    // Background threading
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
